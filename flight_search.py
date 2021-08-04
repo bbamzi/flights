5629e0c2550655f4c5777bb30e4253e9c2306842
akinbodebams <bodejuniorforever@gmail.com>
2021-08-04 08:30:39 -0700

    extracted useful information from the api response , created a function that returns the total time it will take passenger to arrive at destination , aldso created a function with return the total ticket cost if baggage is more than allowed kg given

--- flight_search.py
+++ flight_search.py
@@ -22,37 +22,3 @@ class FlightSearch:
 
 
 
-flight = FlightSearch('PAR').response['data'][0]
-
-
-# departure_city = flight['cityFrom']
-# departure_time = flight['local_departure'].split('T')[1][:-8]
-# destination_city = (flight['cityTo'])
-# destination_country = flight['countryTo']['name']
-# stop_overs = flight['route'][0]['cityFrom']
-# seat_availability = flight['availability']
-# total_time_travel = flight['duration']['total']
-# price = flight['price']
-# handbag_weight_limit = flight['baglimit']['hand_weight']
-# bag_weight_limit = flight['baglimit']['hold_weight']
-# checking_bag_price = flight['bags_price']['1']
-# arrival_date = flight['local_arrival'].split('T')[0]
-# arrival_time = flight['local_arrival'].split('T')[1][:-8]
-# LAYOVER =
-#
-#
-# print(flight)
-# print('departure city: ',departure_city)
-# print('departure time',departure_time)
-# print('destination city',destination_city)
-# print('destination country', destination_country)
-# print('seat left',seat_availability)
-# print(f'total time {round(total_time_travel / 3600)}:{str(total_time_travel % 3600)[:-2]}')
-# print('handbag weight limit',handbag_weight_limit)
-# print('major bag limit',bag_weight_limit)
-# print('price',price)
-# print('checked bag price:',checking_bag_price )
-# print('destination arrival date: ',arrival_date)
-# print('destination arrival time: ', arrival_time)
-
-print(flight)
