0480ef2b60898950c1576a13855b17c1e1df6132
akinbodebams <bodejuniorforever@gmail.com>
2021-08-05 07:41:19 -0700

    created a twilio sms notification with a message attribute "message " that receives generated texts from flight_data.py

---
+++ notification_manager.py
@@ -0,0 +1,28 @@
+from twilio.rest import Client
+
+TOKEN = 'AC964a8ed0a992f34c18c7c1d6181c9afa'
+AUTH = '746d28ab1dd020be9d53d5d9320f2e9c'
+PHONE_NUMBER = '+13126260133'
+
+
+
+
+class NotificationManager:
+
+    def __init__(self, num):
+        message = self.message_reciever('')
+        self.num = num
+        self.client = Client(TOKEN, AUTH)
+        self.message = self.client.messages.create(
+                                  body=message,
+                                  from_=PHONE_NUMBER,
+                                  to=self.num
+                              )
+    def message_reciever(self,message):
+        return message
+
+
+
+
+
+
